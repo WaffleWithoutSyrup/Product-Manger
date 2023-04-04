f86ecc9921747d73a7fff569d07ad23cfc853218
Gabby <sad.duck.club3@gmail.com>
2023-04-03 22:01:32 -0500

    primera prueba

---
+++ productManager.js
@@ -0,0 +1,26 @@
+class ProductManager {
+    constructor(){
+        this.productList = [];
+    }
+    getEventos = ()=>{
+        return this.productList;
+    }
+    addProduct = (title,description,price,thumbnail,code,stock) =>{
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        if (this.productList.lenght===0) {
+            product.id =1;
+        }
+        else{
+            product.id = this.productList[this.productList.lenght-1].id+1;
+        }
+    }
+}
+const productManagerTest =new ProductManager();
+productManagerTest. addProduct
